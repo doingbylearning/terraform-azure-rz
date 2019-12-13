263954d7bf2cb6f102b4079c5c2567e77ff55ca7
konstapel <konstapel@gmx.at>
2019-12-14 00:54:09 +0100

    adding dependencies to ressource and data blocks

--- modules/resource/main.tf
+++ modules/resource/main.tf
@@ -22,6 +22,8 @@ resource "azurerm_network_security_group" "nsg_virtualnetwork_1" {
     name                                = "nsg_virtualnetwork_1"
     location                            = azurerm_resource_group.resourcegroup_terraform.location
     resource_group_name                 = azurerm_resource_group.resourcegroup_terraform.name
+
+    depends_on                          = [azurerm_resource_group.resourcegroup_terraform]
 }
 
 resource "azurerm_virtual_network" "vnet_main_1" {
@@ -29,7 +31,9 @@ resource "azurerm_virtual_network" "vnet_main_1" {
     location                            = azurerm_resource_group.resourcegroup_terraform.location
     resource_group_name                 = azurerm_resource_group.resourcegroup_terraform.name
     address_space                       = var.addressspace_main
-    dns_servers                         = var.dnsservers    
+    dns_servers                         = var.dnsservers   
+
+    depends_on                          = [azurerm_resource_group.resourcegroup_terraform] 
 }
 
 resource "azurerm_subnet" "public_subnets" {
@@ -40,12 +44,16 @@ resource "azurerm_subnet" "public_subnets" {
     address_prefix                      = element(var.public_subnet_cidr, count.index)
     #network_security_group_id           = azurerm_network_security_group.nsg_virtualnetwork_1.id
     #route_table_id                      = azurerm_route_table.rt_public.id
+
+    depends_on                          = [azurerm_virtual_network.vnet_main_1]
 }
 
 resource "azurerm_route_table" "rt_public" {
     name                                = "rt_public"
     location                            = azurerm_resource_group.resourcegroup_terraform.location
     resource_group_name                 = azurerm_resource_group.resourcegroup_terraform.name
+
+    depends_on                          = [azurerm_resource_group.resourcegroup_terraform]
 }
 
 data "azurerm_subnet" "data_public_subnet_ids" {
@@ -53,6 +61,8 @@ data "azurerm_subnet" "data_public_subnet_ids" {
     name                                = format("public-subnet-%s-%d", azurerm_virtual_network.vnet_main_1.name, count.index)
     virtual_network_name                = azurerm_virtual_network.vnet_main_1.name
     resource_group_name                 = azurerm_resource_group.resourcegroup_terraform.name
+
+    depends_on                          = [azurerm_subnet.public_subnets]
 }
 
 
@@ -60,10 +70,14 @@ resource "azurerm_subnet_network_security_group_association" "subnet_nsg_assoc_p
     count                               = length(data.azurerm_subnet.data_public_subnet_ids)
     network_security_group_id           = azurerm_network_security_group.nsg_virtualnetwork_1.id
     subnet_id                           = data.azurerm_subnet.data_public_subnet_ids.*.id[count.index]
+
+    depends_on                          = [data.azurerm_subnet.data_public_subnet_ids]
 }
 
 resource "azurerm_subnet_route_table_association" "subnet_rt_assoc_public" {
     count                               = length(data.azurerm_subnet.data_public_subnet_ids)
     subnet_id                           = data.azurerm_subnet.data_public_subnet_ids.*.id[count.index]
     route_table_id                      = azurerm_route_table.rt_public.id
+
+    depends_on                          = [data.azurerm_subnet.data_public_subnet_ids]
 }
